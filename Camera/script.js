f7f82b7a4de47a3925c9123ceabf3efd203cf81a
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-08-16 23:38:58 +0530

    Add files via upload

---
+++ Camera/script.js
@@ -0,0 +1,7 @@
+let video = document.querySelector("video");
+
+let constraints={
+    video: true,
+    audio: true
+}
+// navigator.mediaDevices.getUserMedia(constraints).then((stream)=>{video.srcObject = stream})
