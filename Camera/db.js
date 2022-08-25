e2eb8367440cfa4f9453b397985cebf4660dd643
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-08-26 00:13:42 +0530

    Update db.js

--- Camera/db.js
+++ Camera/db.js
@@ -2,7 +2,8 @@ let db;
 let openRequest = indexedDB.open("myDataBase");
 
 openRequest.addEventListener("success",(e)=>{
-
+console.log("Db success");
+db = openRequest.result;
 })
 
 openRequest.addEventListener("error",(e)=>{
@@ -13,4 +14,7 @@ openRequest.addEventListener("error",(e)=>{
 
 openRequest.addEventListener("upgradeneeded",(e)=>{
     console.log("DB upgradeneeded");
+    db = openRequest.result;
+    db.createObjectStore("video",{keyPath:"id"});
+    db.createObjectStore("image",{keyPath:"id"});
 })
